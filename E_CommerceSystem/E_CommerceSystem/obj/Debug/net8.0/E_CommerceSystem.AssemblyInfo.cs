6f465d31e8abc1d8558bcfff065b3cc3ae6d2d7a
OmarHamedah <omar.hamedah97@gmail.com>
2024-11-17 11:02:58 +0300

    ignore unneeded

--- E_CommerceSystem/E_CommerceSystem/obj/Debug/net8.0/E_CommerceSystem.AssemblyInfo.cs
+++ E_CommerceSystem/E_CommerceSystem/obj/Debug/net8.0/E_CommerceSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E_CommerceSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8108eb69c14c170e0fd85b5297be0beb406c60f")]
 [assembly: System.Reflection.AssemblyProductAttribute("E_CommerceSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E_CommerceSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
